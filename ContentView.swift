f025e4ddacfab3834fb3c7f7ba66644d5e05c495
Roberto <rgomez9148@stu.d214.org>
2026-04-21 10:57:03 -0500

    created a navigation stack

--- ContentView.swift
+++ ContentView.swift
@@ -2,17 +2,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+    
         VStack {
             Text("Welcome to")
                 .font(.largeTitle)
-                Spacer()
+            Spacer()
             
             Text("Travel Palace")
                 .font(.custom("HelveticaNeue-Bold", size: 50))
                 .frame(maxHeight: .infinity, alignment: .top)
             
-           
-                
+        }
+        NavigationStack{
             
         }
     }
