117be1ec4f4aacba47e8d9fd8b3debfe8d9d7cb6
Roberto <rgomez9148@stu.d214.org>
2026-04-21 11:12:06 -0500

    addedd the navigation stack and the button to see map

--- ContentView.swift
+++ ContentView.swift
@@ -2,32 +2,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    
-        VStack {
-            Text("Welcome to")
-                .font(.largeTitle)
-            Spacer()
-            
-            Text("Travel Palace")
-                .font(.custom("HelveticaNeue-Bold", size: 50))
-                .frame(maxHeight: .infinity, alignment: .top)
-            
-        }
         NavigationStack{
-           
-            Button(action: {
-                print("Map tapped")
-            }) {
-                Text("Map")
+            VStack {
+                Text("Welcome to")
                     .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, minHeight: 500)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(20)
-                    .padding()
+                
+                Text("Travel Palace")
+                    .font(.custom("HelveticaNeue-Bold", size: 50))
+                    .frame(alignment: .top)
+                                
+                NavigationLink(destination: MapView()) {
+                    
+                        Text("See Map")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, minHeight: 100)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                }
             }
-            
         }
     }
 }
