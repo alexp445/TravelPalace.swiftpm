c071f46e03f6eed728181c2dbe7536f05a52f526
Roberto <rgomez9148@stu.d214.org>
2026-04-23 10:37:44 -0500

    changed the mapp to a globe

--- MapView.swift
+++ MapView.swift
@@ -2,18 +2,22 @@ import SwiftUI
 import MapKit
 struct MapView: View {
     @State var address: String = ""
+    
     var body: some View {
         NavigationView {
             VStack{
                 if #available(iOS 17.0, *) {
                     Map()
+                        .mapStyle(.imagery(elevation: .realistic))
                         .edgesIgnoringSafeArea(.all)
                 }
+                    
             }
            
             .toolbar {
                 ToolbarItem(placement: .automatic) {
                     TextField("Search Your Vacation", text: $address)
+                    
                 }
                 
             }
