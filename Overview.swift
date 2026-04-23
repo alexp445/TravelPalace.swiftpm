e00997b256651386137f3d4e2227ac395a46073d
AlexP <alikpritsker@gmail.com>
2026-04-23 11:03:47 -0500

    Made a navigation link in overview to go to the attractions page.

--- Overview.swift
+++ Overview.swift
@@ -1,6 +1,15 @@
 import SwiftUI
 struct Overview: View {
     var body: some View {
-        
+        NavigationLink(destination: Attractions()) {
+            
+            Text("See Attractions")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, minHeight: 100)
+                .background(Color.black)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+        }
     }
 }
