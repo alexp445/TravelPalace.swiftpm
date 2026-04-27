df9b670ede59d2003b696cd125b5a79a3512af44
Danylo Pivtorak <dpivtorak9468@stu.d214.org>
2026-04-27 10:34:36 -0500

    added a city attractions button

--- Attractions.swift
+++ Attractions.swift
@@ -1,16 +1,26 @@
 import SwiftUI
 struct Attractions: View {
     var body: some View {
-        Spacer()
-        NavigationLink(destination: Resorts()) {
+        VStack{
             
-            Text("See Resorts/Hotels")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, minHeight: 100)
-                .background(Color.black)
-                .foregroundColor(.white)
-                .cornerRadius(20)
+            Text("City Attractions:")
+                .font(.custom("HelveticaNeue-Bold", size: 50))
+                .frame(alignment: .top)
+            
+            
+            
+            
+            Spacer()
+            NavigationLink(destination: Resorts()) {
+                
+                Text("See Resorts/Hotels")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, minHeight: 100)
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+            }
         }
     }
 }
