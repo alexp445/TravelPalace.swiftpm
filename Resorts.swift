086b0e3ce82b2d85cc650347c3aa563fcad8f83a
Roberto <rgomez9148@stu.d214.org>
2026-04-27 10:38:32 -0500

    fixed something

--- Resorts.swift
+++ Resorts.swift
@@ -2,15 +2,19 @@ import SwiftUI
 struct Resorts: View {
     var body: some View {
         Text("Hello")
-        NavigationLink(destination: FinishView()) {
-            
-            Text("Finish")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, minHeight: 100)
-                .background(Color.black)
-                .foregroundColor(.white)
-                .cornerRadius(20)
+        if #available(iOS 17.0, *) {
+            NavigationLink(destination: ContentView()) {
+                
+                Text("Finish")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, minHeight: 100)
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+            }
+        } else {
+            // Fallback on earlier versions
         }
     }
 }
