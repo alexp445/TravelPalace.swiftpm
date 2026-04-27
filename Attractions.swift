cdaae46ebc87f636ee316dab61e0ed83a5a62828
AlexP <alikpritsker@gmail.com>
2026-04-27 10:24:09 -0500

    made a navagation link to the resorts page.

--- Attractions.swift
+++ Attractions.swift
@@ -1,6 +1,16 @@
 import SwiftUI
 struct Attractions: View {
     var body: some View {
-        
+        Spacer()
+        NavigationLink(destination: Resorts()) {
+            
+            Text("See Resorts/Hotels")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, minHeight: 100)
+                .background(Color.black)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+        }
     }
 }
