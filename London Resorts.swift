c736461a00322799a7d42bd4806ac22a33c72a0e
Danylo Pivtorak <dpivtorak9468@stu.d214.org>
2026-05-11 10:02:12 -0500

    finished the resorts page for london

--- London Resorts.swift
+++ London Resorts.swift
@@ -9,20 +9,20 @@ struct LondonResorts: View {
             
             Divider()
             
-            Image("")
+            Image("LondonHotel1")
                 .resizable()
                 .frame(width: 150, height: 150)
             
-            Text("")
+            Text("Locke London Canary Wharf is a modern aparthotel in London’s Canary Wharf district, offering stylish studios and suites with kitchens, living spaces, and contemporary design. The hotel features panoramic city views, a rooftop restaurant and bar, fitness facilities, and coworking spaces, making it ideal for both business and leisure travelers. Its location near Canary Wharf Underground and Elizabeth Line stations provides easy access to central London attractions.")
             
             
             Divider()
             
-            Image("")
+            Image("ZHotel")
                 .resizable()
                 .frame(width: 150, height: 150)
             
-            Text("T")
+            Text("The Z Hotel Strand is a modern boutique hotel in central , located near Covent Garden, Trafalgar Square, and the West End theatres. The hotel is known for its compact yet stylish rooms, convenient location, and comfortable amenities such as free Wi-Fi, air conditioning, and smart TVs. It is a popular choice for travelers who want an affordable stay close to London’s main attractions and transport links.")
             
             
             
