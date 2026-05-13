38135edf11f6f2250b5225930e0ed4fb001a30a1
AlexP <alikpritsker@gmail.com>
2026-05-13 11:00:25 -0500

    finshed tokyo overview

--- Tokyo Attractions.swift
+++ Tokyo Attractions.swift
@@ -1,6 +1,30 @@
 import SwiftUI
 struct TokyoAttractions: View {
     var body: some View {
-        
+        VStack{
+            
+            Text("Tokyo Attractions:")
+                .font(.custom("HelveticaNeue-Bold", size: 45))
+                .frame(alignment: .top)
+            
+            Divider()
+            
+            Image("")
+                .resizable()
+                .frame(width: 150, height: 150)
+            
+            Text(".")
+            
+            Divider()
+                
+            Image("")
+                .resizable()
+                .frame(width: 150, height: 150)
+            
+            Text(".")
+            
+            
+            Spacer()
+        }
     }
 }
