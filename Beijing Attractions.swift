0560f85f93e7fab099feaa17aecca4c8d473c45f
AlexP <alikpritsker@gmail.com>
2026-05-15 11:00:09 -0500

    made the city beijing. incuding overview, attractions, and resorts.

--- Beijing Attractions.swift
+++ Beijing Attractions.swift
@@ -9,19 +9,19 @@ struct BeijingAttractions: View {
             
             Divider()
             
-            Image("")
+            Image("Palace")
                 .resizable()
                 .frame(width: 150, height: 150)
             
-            Text(".")
+            Text("The Palace Museum. Located in the very heart of the city, this massive palace complex served as the imperial residence for 24 emperors over 500 years. It is a masterpiece of ancient Chinese architecture, featuring nearly 1,000 buildings filled with royal treasures and historical exhibits")
             
             Divider()
                 
-            Image("")
+            Image("Wall")
                 .resizable()
                 .frame(width: 150, height: 150)
             
-            Text(".")
+            Text("Great Wall of China. This well-preserved section of the wall offers breathtaking views as it winds along forested mountain ridges north of the city. It is particularly popular for being less crowded than other sections and features a fun toboggan ride for the descent.")
             
             
             Spacer()
