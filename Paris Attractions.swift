51c717e0fbbd915e1419bd892d4801a647a7c3b9
AlexP <alikpritsker@gmail.com>
2026-05-19 11:07:07 -0500

    Finished the paris paeges and included everything

--- Paris Attractions.swift
+++ Paris Attractions.swift
@@ -1,6 +1,30 @@
 import SwiftUI
 struct  ParisAttractions: View {
     var body: some View {
-        Text("Paris Attractions")
+        VStack{
+            
+            Text("Paris Attractions:")
+                .font(.custom("HelveticaNeue-Bold", size: 42))
+                .frame(alignment: .top)
+            
+            Divider()
+            
+            Image("Eiffel")
+                .resizable()
+                .frame(width: 150, height: 150)
+            
+            Text("The Eiffel Tower is the most recognizable landmark in Paris and offers sweeping views of the city from its observation decks. Built for the 1889 World’s Fair, it has become a global symbol of France and is especially stunning when illuminated at night.")
+            
+            Divider()
+                
+            Image("Notre")
+                .resizable()
+                .frame(width: 150, height: 150)
+            
+            Text("Notre-Dame is a masterpiece of French Gothic architecture known for its dramatic stained glass windows, gargoyles, and historic significance. Although damaged in a 2019 fire, it remains one of Paris’s most beloved and visited landmarks.")
+            
+            
+            Spacer()
+        }
     }
 }
--- Paris Overview.swift
+++ Paris Overview.swift
@@ -1,6 +1,19 @@
 import SwiftUI
 struct  ParisOverview: View {
     var body: some View {
-        Text("Overview")
+        VStack{
+            Text("Overview of Paris:")
+                .font(.custom("HelveticaNeue-Bold", size: 41))
+                .frame(alignment: .top)
+                .padding(10)
+            
+            Divider()
+            
+            Text("Paris, the capital of France, is a globally renowned metropolis situated along the scenic Seine River in the north-central part of the country. Celebrated as the City of Light it has been a leading international center for fashion, gastronomy, philosophy, and the arts since the 17th century. The city's landscape is famously defined by its grand 19th-century boulevards and iconic historical landmarks, including the Eiffel Tower, the Louvre Museum, and the newly restored Notre-Dame Cathedral. Divided into 20 distinct districts called arrondissements, modern Paris effortlessly blends its rich, centuries-old heritage with a vibrant, contemporary cultural scene.")
+                .font(.custom("Italic Text", size: 25))
+            
+            
+            Spacer()
+        }
     }
 }
